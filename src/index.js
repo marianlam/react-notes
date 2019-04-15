4cd4cdb9e191daae39ababe6781b957d60959b53
marzipanmars <malam@vassar.edu>
2019-04-15 12:56:17 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up!');
+// change require to es6 import style
+// const $ = require('jquery');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+  num += 1;
+}, 1000);
